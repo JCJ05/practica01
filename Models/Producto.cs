a995906d10c1be79d7575abf263d5690e0e65d01
JCJ05 <julitoaguerorengifo@hotmail.com>
2021-08-29 15:30:50 -0500

    Clases y estilos

---
+++ Models/Producto.cs
@@ -0,0 +1,15 @@
+namespace practica01.Models
+{
+    public class Producto
+    {
+        public int Id {get; set;}
+
+        public string nombre {get; set;}
+
+        public string categoria {get; set;}
+
+        public float precio {get; set;}
+
+        public float descuento {get; set;}
+    }
+}
